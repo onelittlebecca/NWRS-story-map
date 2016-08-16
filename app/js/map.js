774186a90f2cb5a5bfc167ea8e6ccedac409d238
Becca <becca.goodman@gmail.com>
2016-08-16 09:44:33 -0400

    Updated 3 files
    Updated files:
    #	app/js/map.js
    #	app/js/splash.js
    #	app/templates/detail.jade

--- app/js/map.js
+++ app/js/map.js
@@ -26,8 +26,6 @@
 
   function registerHandlers() {
     options.fullExtent.addEventListener('click', zoomToFullExtent);
-//     emitter.on('project:click', displayGeography);
-//     emitter.on('geographies:loaded', saveGeographies);
   }
 
   function destroy() {
@@ -62,49 +60,17 @@
       onEachFeature: function(feature, layer) {
         layer.on({ click: onMarkerClick });
       },
-//       pointToLayer: function (feature, latlng) {
-//         var props = feature.properties;
-//         switch(props.theme) {
-//           case 'Smart Planning':
-//             return L.marker(latlng, { icon: icons.orange });
-//           case 'Building on Existing Partnerships':
-//             return L.marker(latlng, { icon: icons.blue });
-//           case 'Saving Dollars and Improving Efficiencies':
-//             return L.marker(latlng, { icon: icons.green });
-//           case 'Achieving Conservation':
-//             return L.marker(latlng, { icon: icons.purple });
-//         }
-//       }
+
     }).addTo(map);
 
     map.fitBounds(options.markers.getBounds(), { paddingBottomRight: [0, 300]});
   }
 
-//   function saveGeographies(geog) {
-//     geographies = geog;
-//   }
 
   function onMarkerClick(e) {
     emitter.emit('project:click', e.target.feature);
   }
 
-//   function displayGeography(office) {
-//     var clientWidth = document.documentElement.clientWidth;
-//     geogLayer.clearLayers();
-//     var currentGeog = L.geoJson(geographies, {
-//       filter: function (feature) {
-//         return feature.properties.name === office.properties.geography;
-//       }
-//     });
-//     geogLayer.addLayer(currentGeog);
-// 
-//     // Decide if we should make room on the map for the infowindow
-//     if (clientWidth > 1000)
-//       map.fitBounds(currentGeog.getBounds(), { paddingBottomRight: [500, 0]});
-//     else
-//       map.fitBounds(currentGeog.getBounds());
-//     emitter.emit('gallery:close');
-//   }
 
   function zoomToFullExtent() {
     map.fitBounds(options.markers.getBounds(), { paddingBottomRight: [0, 300]});
